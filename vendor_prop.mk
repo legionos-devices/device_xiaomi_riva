78e1ad99a7868df3f63c1c1b0e11f80b65e85fcc
Harvey <hhyunjinn21@gmail.com>
2020-07-11 17:40:09 +0700

    riva : add lmdk props
     * Kang lmkd props from newest google devices (aka Pixels).
    
     * This makes lmkd spread its wings and become a multitasking beast over kernel side LMK.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -120,6 +120,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.perf_hint_window=50 \
     vendor.gralloc.enable_fb_ubwc=1
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.lmk.low=1001 \
+ro.lmk.medium=800 \
+ro.lmk.critical=0 \
+ro.lmk.critical_upgrade=false \
+ro.lmk.upgrade_pressure=100 \
+ro.lmk.downgrade_pressure=100 \
+ro.lmk.kill_heaviest_task=true \
+ro.lmk.kill_timeout_ms=100 \
+ro.lmk.use_minfree_levels=true
+
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
